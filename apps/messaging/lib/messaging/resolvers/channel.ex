8159f22d47111b87f9124be3aac6456ee4a87cb4
Christian Bick <christian.bick@bitsuppliers.com>
2021-02-28 14:39:52 +0100

    First serious IO implementation

--- apps/messaging/lib/messaging/resolvers/channel.ex
+++ apps/messaging/lib/messaging/resolvers/channel.ex
@@ -1,12 +1,21 @@
 defmodule Messaging.ChannelResolver do
 
-  def find(_parent, _args, _resolution) do
-    {:ok, %{ id: "1", name: "Channel Name" }}
+  def find(_parent, %{:id => id}, _resolution) do
+    result = Mongo.find_one(:mongo, "channels", %{_id: BSON.ObjectId.decode!(id)})
+    case result do
+      %{"_id" => id, "name" => name} ->
+        {:ok, %{id: BSON.ObjectId.encode!(id), name: name}}
+      nil ->
+        {:error, "Not found"}
+      {:error, error} ->
+        IO.inspect(error)
+        {:error, ["Database Error", error.message ]}
+    end
   end
 
-  def create(_parent, _args, _resolution) do
-    {:ok, result} = Mongo.insert_one(:mongo, "channels", _args)
-    {:ok, %{ id: BSON.ObjectId.encode! result.inserted_id }}
+  def create(_parent, %{:name => name}, _resolution) do
+    {:ok, result} = Mongo.insert_one(:mongo, "channels", %{name: name})
+    {:ok, %{id: BSON.ObjectId.encode!(result.inserted_id), name: name}}
   end
 
 end
